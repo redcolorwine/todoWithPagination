d25c5bbcd8d564a019011a7fc4a0868535a59eeb
Лобец Сергей Александрович <SA.Lobets.2711@fss.local>
2022-03-05 12:39:15 +1000

    commit06

--- src/components/Taskpad/pagination.js
+++ src/components/Taskpad/pagination.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import cmedia from './pagination.module.css';
 const Pagination = (props) => {
     const pageNumbers = [];
-
+    const [activeId, setActiveId] = useState();
     for (let i = 1; i <= Math.ceil(props.totalTasks / props.tasksPerPage); i++) {
         pageNumbers.push(i);
     }
@@ -12,10 +13,11 @@ const Pagination = (props) => {
             <ul>
                 {pageNumbers.map(number => {
                     return (<li key={number} className={cmedia.pageItem} >
-                        <NavLink onClick={(event) => {
+                        <NavLink id={number} className={activeId === number ? cmedia.active : cmedia.pageLink} onClick={(event) => {
+                            setActiveId(number)
                             event.preventDefault();
                             props.paginate(number)
-                        }} to='!#' className={({ isActive }) => `${isActive ? cmedia.active : cmedia.pageLink}`}>
+                        }} to='!#' >
                             {number}
                         </NavLink>
                     </li>)
@@ -24,5 +26,5 @@ const Pagination = (props) => {
         </nav>
     )
 }
-
+// className={({ isActive }) => `${isActive ? cmedia.active : cmedia.pageLink}`} 
 export default Pagination
